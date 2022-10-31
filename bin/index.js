14957d3b612ae96c6bd712b8fbad9fd6381fef58
Nic Ford <nicdford@gmail.com>
2022-10-31 11:19:10 -0600

    Fix whitespaces

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
-const path = require("path")
-const fse = require("fs-extra")
-const cwd = process.cwd()
+const path = require("path");
+const fse = require("fs-extra");
+const cwd = process.cwd();
 
 /*
  * Component Duplicator
@@ -9,32 +9,32 @@ const cwd = process.cwd()
  * @param {string} destComponentName - The name of the component to be created
  * `npx duplicate Button Chip`
  */
-const args = process.argv.slice(2)
-const srcComponentName = args[0]
-const destComponentName = args[1]
+const args = process.argv.slice(2);
+const srcComponentName = args[0];
+const destComponentName = args[1];
 
-const srcDir = path.join(cwd, "src/components", srcComponentName)
-const destDir = path.join(cwd, "src/components", destComponentName)
+const srcDir = path.join(cwd, "src/components", srcComponentName);
+const destDir = path.join(cwd, "src/components", destComponentName);
 
 // Copy the component directory
 try {
-  fse.copySync(srcDir, destDir, { overwrite: true })
+    fse.copySync(srcDir, destDir, { overwrite: true });
 } catch (err) {
-  console.error(err)
+    console.error(err);
 }
 
 // Rename the component files
 fse.readdirSync(destDir).forEach((file) => {
-  if (file.includes(srcComponentName)) {
-    const newFileName = file.replace(srcComponentName, destComponentName)
-    const srcFile = path.join(destDir, file)
-    const destFile = path.join(destDir, newFileName)
+    if (file.includes(srcComponentName)) {
+        const newFileName = file.replace(srcComponentName, destComponentName);
+        const srcFile = path.join(destDir, file);
+        const destFile = path.join(destDir, newFileName);
 
-    fse.rename(srcFile, destFile, (error) => {
-      if (error) console.log(error)
+        fse.rename(srcFile, destFile, (error) => {
+            if (error) console.log(error);
 
-      // List all the filenames after renaming
-      console.log(`🎉  ${newFileName} Renamed Successfully!`)
-    })
-  }
-})
+            // List all the filenames after renaming
+            console.log(`🎉  ${newFileName} Renamed Successfully!`);
+        });
+    }
+});
